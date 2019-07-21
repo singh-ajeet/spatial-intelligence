f7bbb3ecec317d6607e0084eee2222bff55e842e
Ajeet Singh <ajeetsinghtomer@gmail.com>
2019-07-21 18:04:53 +0530

    Files added back

---
+++ services/location-service/src/main/java/com/spi/platform/spatial/geometryservice/GeometryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.spi.platform.spatial.geometryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeometryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeometryServiceApplication.class, args);
+	}
+
+}
